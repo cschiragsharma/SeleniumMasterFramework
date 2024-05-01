3dad1b66ab1b9bc664f8e6bd2e3d0142f0d60ba9
cschiragsharma <cschiragsharma@gmail.com>
2024-05-01 13:33:30 +0530

    trying page factory

--- src/test/java/org/selenium/pom/pages/CartPage.java
+++ src/test/java/org/selenium/pom/pages/CartPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.selenium.pom.base.BasePage;
 
@@ -19,6 +20,7 @@ public class CartPage extends BasePage {
 
     public CartPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public Boolean isLoaded(){
