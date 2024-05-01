24c1f6ed92fd068056e3bc76981383c0499cb648
cschiragsharma <cschiragsharma@gmail.com>
2024-05-01 16:47:08 +0530

    Making it cross browser and adding browser names as ENUMs

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -3,11 +3,24 @@ package org.selenium.pom.factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.selenium.pom.constants.DriverType;
 
 public class DriverManager {
     public WebDriver initializeDriver(){
-        WebDriverManager.chromedriver().cachePath("Drivers").setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver;
+        String browser = System.getProperty("browser","CHROME");
+        driver = switch (DriverType.valueOf(browser)) {
+            case CHROME -> {
+                WebDriverManager.chromedriver().cachePath("Drivers").setup();
+                yield new ChromeDriver();
+            }
+            case FIREFOX -> {
+                WebDriverManager.firefoxdriver().cachePath("Drivers").setup();
+                yield new FirefoxDriver();
+            }
+            default -> throw new IllegalStateException("Invalid Browser Name: " + browser);
+        };
 
         driver.manage().window().maximize();
        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
