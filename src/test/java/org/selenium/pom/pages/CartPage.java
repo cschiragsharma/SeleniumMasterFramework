bcb7d1f51a6487b327b87d4c0513d31d04189f3c
cschiragsharma <cschiragsharma@gmail.com>
2024-05-01 15:47:34 +0530

    going back to page object

--- src/test/java/org/selenium/pom/pages/CartPage.java
+++ src/test/java/org/selenium/pom/pages/CartPage.java
@@ -2,7 +2,6 @@ package org.selenium.pom.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,17 +9,17 @@ import org.selenium.pom.base.BasePage;
 
 public class CartPage extends BasePage {
 
-//    private final By productName = By.cssSelector("td[class='product-name'] a");
-//    private final By clickCheckoutBtn = By.cssSelector(".checkout-button");
-//    private final By cartHeading = By.cssSelector(".has-text-align-center");
+    private final By productName = By.cssSelector("td[class='product-name'] a");
+    private final By clickCheckoutBtn = By.cssSelector(".checkout-button");
+    private final By cartHeading = By.cssSelector(".has-text-align-center");
     //page factory
-    @FindBy(css = "td[class='product-name'] a") private By  productName;
-    @FindBy(css = ".checkout-button") private By clickCheckoutBtn;
-    @FindBy(css = ".has-text-align-center") private By cartHeading;
+//    @FindBy(css = "td[class='product-name'] a") private By  productName;
+//    @FindBy(css = ".checkout-button") private By clickCheckoutBtn;
+//    @FindBy(css = ".has-text-align-center") private By cartHeading;
 
     public CartPage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(driver, this);
+        //PageFactory.initElements(driver, this);
     }
 
     public Boolean isLoaded(){
