bca55aa3695c8fefb8cd2732d0ab260a101d40a2
cschiragsharma <cschiragsharma@gmail.com>
2024-05-01 13:32:10 +0530

    trying page factory

--- src/test/java/org/selenium/pom/pages/CartPage.java
+++ src/test/java/org/selenium/pom/pages/CartPage.java
@@ -2,14 +2,21 @@ package org.selenium.pom.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.selenium.pom.base.BasePage;
 
 public class CartPage extends BasePage {
 
-    private final By productName = By.cssSelector("td[class='product-name'] a");
-    private final By clickCheckoutBtn = By.cssSelector(".checkout-button");
-    private final By cartHeading = By.cssSelector(".has-text-align-center");
+//    private final By productName = By.cssSelector("td[class='product-name'] a");
+//    private final By clickCheckoutBtn = By.cssSelector(".checkout-button");
+//    private final By cartHeading = By.cssSelector(".has-text-align-center");
+    //page factory
+    @FindBy(css = "td[class='product-name'] a") private By  productName;
+    @FindBy(css = ".checkout-button") private By clickCheckoutBtn;
+    @FindBy(css = ".has-text-align-center") private By cartHeading;
+
     public CartPage(WebDriver driver) {
         super(driver);
     }
