22ccca9435eb73684c4f45ad09e14a3380f82a66
cschiragsharma <cschiragsharma@gmail.com>
2024-05-04 17:31:19 +0530

    GET and POST implemented for API

--- src/test/java/org/selenium/pom/api/actions/SignUpApi.java
+++ src/test/java/org/selenium/pom/api/actions/SignUpApi.java
@@ -1,15 +1,22 @@
 package org.selenium.pom.api.actions;
 
+import com.google.common.collect.HashBiMap;
+import groovy.lang.GroovyObjectSupport;
 import io.restassured.http.Cookies;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.selenium.pom.objects.LoginUser;
 import org.selenium.pom.utils.configLoader;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 
-public class SignUpApi {
+public class SignUpApi extends GroovyObjectSupport {
     private Cookies cookies;
 
     public Cookies getCookies() {
@@ -20,12 +27,20 @@ public class SignUpApi {
         //groovy Gpath
         return response.htmlPath().getString("**.findAll { it.@name =='woocommerce-register-nonce' }.@value");
     }
-    private String fetchRegisterNonceValueUsingJsoup(){
+    public String fetchRegisterNonceValueUsingJsoup(){
         Response response = getAccount();
         Document doc = Jsoup.parse(response.body().prettyPrint());
         Element element = doc.selectFirst("#woocommerce-register-nonce");
+        assert element != null;
         return element.attr("value");
 
+    }
+    private String fetchFormNameUsingJsoup(){
+        Response response = getAccount();
+        Document doc = Jsoup.parse(response.body().prettyPrint());
+        Element element = doc.selectFirst("button[value='Register']");
+        assert element != null;
+        return element.attr("value");
     }
     private Response getAccount(){
         Cookies cookies = new Cookies();
@@ -42,5 +57,33 @@ public class SignUpApi {
            throw new RuntimeException("Failed to fetch the account, HTTP Status code: " + response.getStatusCode());
        }
        return response;
+    }
+    public Response register(LoginUser user){
+        Cookies cookies = new Cookies();
+        Header header = new Header("content-type","application/x-www-form-urlencoded");
+        Headers headers = new Headers(header);
+        HashMap<String,String> formParams = new HashMap<>();
+        formParams.put("username", user.getUsername());
+        formParams.put("email", user.getEmail());
+        formParams.put("password", user.getPassword());
+        formParams.put("woocommerce-register-nonce", fetchRegisterNonceValueUsingJsoup());
+        formParams.put("register",fetchFormNameUsingJsoup());
+        Response response = given().
+                baseUri(configLoader.getInstance().getBaseUrl()).
+                headers(headers).
+                formParams(formParams).
+                cookies(cookies).
+                log().all().
+        when().
+                post("/account").
+        then().
+                log().all().
+                extract().
+                response();
+        if(response.getStatusCode()!=302){
+            throw new RuntimeException("Failed to register the account, HTTP Status code: " + response.getStatusCode());
+        }
+        this.cookies = response.getDetailedCookies();
+        return response;
     }
 }
--- src/test/java/org/selenium/pom/objects/LoginUser.java
+++ src/test/java/org/selenium/pom/objects/LoginUser.java
@@ -3,28 +3,49 @@ package org.selenium.pom.objects;
 public class LoginUser {
     private String username;
     private String password;
+    private String email;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public LoginUser setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public LoginUser() {
+    }
 
     public LoginUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public LoginUser(String username, String password, String email) {
 
                this.username = username;
                this.password = password;
+               this.email = email;
 
     }
+
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
+    public LoginUser setUsername(String username) {
         this.username = username;
+        return this;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public LoginUser setPassword(String password) {
         this.password = password;
+        return this;
     }
 }
